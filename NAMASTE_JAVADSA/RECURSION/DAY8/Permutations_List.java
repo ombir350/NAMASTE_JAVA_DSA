e903b0bae36985c0e19af3cc760350038bc6927d
Ombir Kumar <your_github_email@example.com>
2026-09-04 15:07:39 +0530

    Add the Day8 Recursion programs - Permutations (Theory + Code + Tips).

---
+++ NAMASTE_JAVADSA/RECURSION/DAY8/Permutations_List.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Permutations_List {
+    public static ArrayList<String> permutation(String p,String up){
+        if (up.isEmpty()){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> ans=new ArrayList<>();
+        for (int i=0;i<=p.length();i++){
+            String f=p.substring(0,i);
+            String s=p.substring(i,p.length());
+            ans.addAll(permutation(f+ch+s,up.substring(1)));
+        }
+        return ans;
+    }
+    public static void main (String []args){
+        ArrayList<String> ans=permutation("","abc");
+        System.out.print(ans);
+    }
+}
---
+++ NAMASTE_JAVADSA/RECURSION/DAY8/ombir.java
@@ -0,0 +1,19 @@
+public class ombir{
+    public static int permutation(String p,String up){
+        if (up.isEmpty()){
+            return 1;
+        }
+        int count=0;
+        char ch=up.charAt(0);
+        for (int i=0;i<=p.length();i++){
+            String f=p.substring(0,i);
+            String s=p.substring(i,p.length());
+            count=count+permutation(f+ch+s,up.substring(1));
+        }
+        return count;
+    }
+    public static void main (String []args){
+       int ans= permutation("","abdc");
+        System.out.print(ans);
+    }
+}
