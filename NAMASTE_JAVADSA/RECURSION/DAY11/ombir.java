baec6a4148c31fc186842d6c5e8c9af722789f4e
Ombir Kumar <191267158+ombir350@users.noreply.github.com>
2026-09-20 23:33:58 +0530

    Add the Day11 of the recurtion problem N-Queens, N-Knights, Sudoku Solver

---
+++ NAMASTE_JAVADSA/RECURSION/DAY11/ombir.java
@@ -0,0 +1,72 @@
+public class NQueens {
+
+    static boolean isSafe(char[][] board, int row, int col) {
+
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static boolean solve(char[][] board, int row) {
+
+        if (row == board.length) {
+            return true;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+
+            if (isSafe(board, row, col)) {
+
+                board[row][col] = 'Q';
+
+                if (solve(board, row + 1)) {
+                    return true;
+                }
+
+                board[row][col] = '.';
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        solve(board, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
